8645fce0708e65e665c17062199cec7df5e45f94
bbspn <660560792@KMITL.AC.TH>
2023-10-09 17:21:25 +0700

    Bright

--- src/ex06.py
+++ src/ex06.py
@@ -3,7 +3,23 @@ Execise 6
 """
 
 
-def ordinal_suffix(param):
+def ordinal_suffix(num):
+    numstr = str(num)
+
+    if numstr[-2:] in ("11","12","13"):
+        return numstr + "th"
+    elif numstr[-1] == "1":
+        return numstr + "st"
+    elif numstr[-1] == "2":
+        return numstr + "nd"
+    elif numstr[-1] == "3":
+        return numstr + "rd"
+    else:
+        return numstr + "th"
+
+
+
+
     """
     Return the ordinal suffix for a given number.
 
@@ -13,5 +29,6 @@ def ordinal_suffix(param):
     Returns:
         str: The ordinal suffix corresponding to the given number.
     """
-    # TODO : complete this
+    # fix : complete this
     pass
+
