9b7f36d606790e87f294333c47d2fc5bbe814525
bbspn <660560792@KMITL.AC.TH>
2023-10-09 18:12:54 +0700

    Assignment

--- src/ex09.py
+++ src/ex09.py
@@ -4,6 +4,14 @@ Execise 9
 
 
 def get_chess_square_color(col, row):
+    if col <1 or col > 8 or row <1 or row > 8:
+        return " "
+    elif col %2 == row %2:
+        return "White"
+    elif col%2 != row%2:
+        return "Black"
+
+
     """
     Returns the color of a chess square given its column and row.
     The color of the square is determined by checking
