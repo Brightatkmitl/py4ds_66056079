c71fb63d5784a1934abfad02b6df84cb3a351238
bbspn <660560792@KMITL.AC.TH>
2023-10-13 22:55:52 +0700

    Fix Exercise 25 Solutions Complete

--- src/ex23.py
+++ src/ex23.py
@@ -4,6 +4,7 @@ Exercise 23 : 99 Bottles of Beer on the Wall
 
 
 def bottles_of_beer(param):
+
     """
     Calculate the number of bottles of beer on the wall.
     And print out like this :
@@ -21,8 +22,25 @@ def bottles_of_beer(param):
     Returns:
         str: The repeated lyrics of bottles of beer on the wall.
     """
-    # TODO : complete this
-    pass
+    # fix : complete this
+
+    result = ''
+    if param != 1:
+        result += str(param) + ' bottles of beer on the wall,\n'
+        result += str(param) + ' bottles of beer.\n'
+        result += 'Take one down, pass it around,\n'
+        if (param - 1) == 1:
+            result += '1 bottle of beer on the wall.\n'
+        else:
+            result += str(param - 1) + ' bottles of beer on the wall.\n'
+    else:
+        result += '1 bottle of beer on the wall,\n'
+        result += '1 bottle of beer.\n'
+        result += 'Take one down, pass it around,\n'
+        result += 'No more bottles of beer on the wall!\n'
+    return result
+
+pass
 
 
 def loop_bottles_of_bears(bottle):
